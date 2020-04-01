7f53c8af506ee82617a585df89971b04b5d6e0fc
Zack Hayden <signbarn@gmail.com>
2020-04-01 13:21:06 -0400

    showing what our customer data structure will look like

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 
+// table info
+// {
+//     name: "John Doe",
+//     email: "john@example.com",
+//     id: "unique-id",
+//     phone: "404-867-5309"
+// }
+
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(`App listening on https://localhost:${PORT}`);
   });
   
