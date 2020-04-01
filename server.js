9d19293a344de70d4410b69aa8aae56020dfd396
Zack Hayden <signbarn@gmail.com>
2020-04-01 13:36:36 -0400

    add routes, add node dependencies

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 
+function getPathToFile(fileName) {
+    return path.join(__dirname, fileName);
+}
+
+function buildHtmlSend(pageName) {
+    return [`${getPathToFile(pageName)}.html`, {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    }];
+}
 // table info
 // {
 //     name: "John Doe",
@@ -11,6 +22,17 @@ const path = require("path");
 //     phone: "404-867-5309"
 // }
 
+app.get("/", (req, res) => {
+    return res.sendFile(...buildHtmlSend("home"));
+});
+
+app.get("/tables", (req, res) => {
+    return res.sendFile(...buildHtmlSend("tables"));
+});
+
+app.get("/reserve", (req, res) => {
+    return res.sendFile(...buildHtmlSend("reserve"));
+});
 
 app.listen(PORT, function() {
     console.log(`App listening on https://localhost:${PORT}`);
